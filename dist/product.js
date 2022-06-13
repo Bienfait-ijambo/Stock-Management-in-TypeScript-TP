e073996c6b94513d880ed5dbbe76419f9136a5e6
Bienfait-ijambo <ijamboizuba20@gmail.com>
2022-06-13 15:01:48 +0200

    finish crud product

--- dist/product.js
+++ dist/product.js
@@ -9,9 +9,28 @@ class Product {
         this.tblProduct.push(product);
         return this;
     }
+    edit(productId) {
+        const product = this.tblProduct.filter(product => product.id === productId);
+        return product;
+    }
+    update(productName, productPrice) {
+        let { name, price } = this.tblProduct[0];
+        //has name
+        if (productName !== undefined)
+            name = productName;
+        //has price
+        if (productPrice !== undefined)
+            price = productPrice;
+        return this;
+    }
 }
 const product = new Product([]);
 product
     .add({ id: 1, name: "Jus", price: 12 })
     .add({ id: 2, name: "Energie", price: 16 });
+const edit = product.edit(2);
+console.log("here we get product name :" + edit);
+//change name
+edit[0].name = "Energie-max";
 console.log(product);
+console.log(edit);
--- src/product.ts
+++ src/product.ts
@@ -1,30 +1,58 @@
-interface Ifields{
-    id:number,
-    name:string,
-    price:number
+interface Ifields {
+    id: number,
+    name: string,
+    price: number
 }
 
 
-class Product   {
+class Product {
 
-    private tblProduct:Ifields[]=[]
+    private tblProduct: Ifields[] = []
 
-    constructor(product:any[]){
-        this.tblProduct=product
+    constructor(product: any[]) {
+        this.tblProduct = product
         console.log("product instance is created ...")
     }
 
-    add(product:Ifields){
+    
+    add(product: Ifields) {
         this.tblProduct.push(product)
         return this
     }
 
-    
+    edit(productId: number) {
+        const product = this.tblProduct.filter(product => product.id === productId)
+        return product
+    }
+
+    update(productId: number, productName?: string, productPrice?: number) {
+        const product = this.tblProduct.filter(product => product.id === productId)
+        //has name
+        if (productName !== undefined)
+            product[0].name = productName
+        //has price
+        if (productPrice !== undefined)
+            product[0].price = productPrice
+
+        return this
+    }
+
+    delete(productId: number) {
+        const product = this.tblProduct.filter(product => product.id !== productId)
+        return product
+    }
+
+
 }
 
-const product=new Product([])
+const product = new Product([])
 
 product
-.add({id:1,name:"Jus",price:12})
-.add({id:2,name:"Energie",price:16})
+    .add({ id: 1, name: "Jus", price: 12 })
+    .add({ id: 2, name: "Energie", price: 16 })
+
+
+product.update(2, "Energie-plus-plus")
+
 console.log(product)
+
